8278d7ef749dcdcd59791b04cd8bf50118f13b53
YanMo <xiajiaruimail@qq.com>
2024-03-22 06:03:24 +0800

    Add | 下载进度条

--- download.py
+++ download.py
@@ -1,40 +1,28 @@
 import platform
 import requests
-import subprocess
-import winreg
-import zipfile
+import os
+
+from tqdm import tqdm
 
 
 # 封装查询系统架构的函数
-def get_system_architecture():
-    try:
-        key_name = r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
-        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_name) as key:
-            arch_value, _ = winreg.QueryValueEx(key, "PROCESSOR_ARCHITECTURE")
-        return arch_value.lower()
-    except Exception as err:
-        print(f"不支持此架构: {err}")
-        return None
-
-
-# 检测系统架构并下载对应文件
-def check_system_architecture():
-    os_name = platform.system()
-    arch_info = get_system_architecture()
-
-    if os_name != "Windows" or arch_info is None:
-        print("不支持的操作系统或无法获取处理器架构")
-        return
-
-    print(f"检测到系统环境是 {arch_info}，正在下载适用于 {arch_info} 的软件包...")
-    requests.get(f"https://mirrors.nyanest.xyz/locyan/{arch_info}_windows.zip", stream=True)
-
-    def unzip_file(zip_path, target_path):
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(target_path)
-
-    unzip_file(f'{arch_info}_windows.zip', './LoCyanPureFrp')
-
-
-# 调用检查函数
-check_system_architecture()
+def download_frpc():
+    if os.path.exists('frpc'):
+        os_name = platform.system()
+        if os_name.lower() == 'windows':
+            windows_downloader = requests.get('https://mirrors.nyanest.xyz/locyan/frpc.exe', stream=True)
+            total = int(windows_downloader.headers.get('content-length', 0))
+            with open('frpc.exe', 'wb') as file, tqdm(
+                    desc='frpc.exe',
+                    total=total,
+                    unit='iB',
+                    unit_scale=True,
+                    unit_divisor=1024,
+            ) as bar:
+                for data in windows_downloader.iter_content(chunk_size=1024):
+                    size = file.write(data)
+                    bar.update(size)
+        elif os_name.lower() == 'linux':
+            requests.get('https://mirrors.nyanest.xyz/locyan/frpc', stream=True)
+        else:
+            print("暂不支持此架构")
